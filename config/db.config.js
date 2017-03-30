2e34a9f247c7f737f644f41acc02c60bfda71fcd
Hai Phan <danghaiphan1210@gmail.com>
2017-03-30 23:24:19 +0300

    .env edited

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,4 @@
-const databaseName = 'boardgame';
+const databaseName = 'heroku_hnndcxg4';
 
 module.exports = {
     connectionUrl: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${databaseName}`
